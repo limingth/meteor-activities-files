aa76033cc29f4cb995d96b5453774b41a2a11f2c
dououFullstack <13412133311@139.com>
2015-10-28 14:07:40 +0800

    Add css for activity_details_page, Use jss:flat-uithen  the effect is better

--- client/activities/activity_show.js
+++ client/activities/activity_show.js
@@ -6,3 +6,45 @@ Template.activityShow.helpers({
 
 Template.activityShow.events ({
 });
+
+Template.registerHelper('formatdatetime', function(datetime){
+  if (moment && datetime) {
+    if(datetime.getDate() === new Date().getDate()){
+      return "今天 " + moment(datetime).format("hh:mm");
+    } else{
+      return moment(datetime).format("YYYY/MM/DD hh:mm");
+    }
+
+  }
+  else {
+    return datetime;
+  }
+});
+
+Template.registerHelper('formatdatetimeTime', function(datetime){
+  if (moment && datetime) {
+    if(datetime.getDate() === new Date().getDate()){
+      return "今天 " + moment(datetime).format("hh:mm");
+    } else{
+      return moment(datetime).format("hh:mm");
+    }
+
+  }
+  else {
+    return datetime;
+  }
+});
+
+Template.registerHelper('formatdatetimeDate', function(datetime){
+  if (moment && datetime) {
+    if(datetime.getDate() === new Date().getDate()){
+      return "今天 " + moment(datetime).format("hh:mm");
+    } else{
+      return moment(datetime).format("YYYY-MM-DD");
+    }
+
+  }
+  else {
+    return datetime;
+  }
+});
